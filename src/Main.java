01dccd6276f74ca8862b0629146c9b057acdf67d
margaZM <aurazambrano227@gmail.com>
2022-06-05 15:02:52 -0500

    Add second exercise openBootcamp

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+        // first exercise:
+        int numberIf = 1;
+        if (numberIf == 0)  {
+            System.out.println("is zero");
+            return;
+        } else if (numberIf > 0) {
+            System.out.println("es positive");
+            return;
+        }
+        System.out.println("es negative");
+
+
+        // second exercise
+        int numberWhile = 1;
+        while (numberWhile < 3 ) {
+            numberWhile++;
+            System.out.println(numberWhile);
+        }
+
+        // third exercise
+        do {
+            numberWhile++;
+            System.out.println(numberWhile);
+        } while (numberWhile < 3 );
+
+        // four exercise
+        for (int count = 0; count <= 3; count++) {
+            System.out.println(count);
+        }
+
+        // five exercise
+        String season = "spring";
+        switch (season) {
+            case "spring":
+                System.out.println("It's spring");
+                break;
+            case "summer":
+                System.out.println("It's summer");
+                break;
+            case "winter":
+                System.out.println("It's winter");
+                break;
+            case "fall":
+                System.out.println("It's fall");
+                break;
+            default:
+                System.out.println("Invalid season of the year");
+                break;
+        }
+    }
+}
